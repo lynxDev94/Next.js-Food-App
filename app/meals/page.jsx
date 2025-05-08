202c3b5fe6c435dd7efa3318ff4c0ca38f0ac96a
lynxDev94 <lynxpounceweb@gmail.com>
2025-05-08 08:22:42 +0300

    add meals page

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import classes from "./page.module.css";
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
 
 function Meals() {
   return (
-    <div>Meals</div>
-  )
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!{" "}
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
 
-export default Meals
+export default Meals;
