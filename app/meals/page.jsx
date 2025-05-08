960511b325f0ba54192d65a545c988af818d1925
lynxDev94 <lynxpounceweb@gmail.com>
2025-05-08 09:31:46 +0300

    add suspense instead of loader.js

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
 
-async function Meals() {
+async function MealsLoader() {
   const meals = await getMeals();
-  
+  return <MealsGrid meals={meals} />;
+}
 
+function Meals() {
   return (
     <>
       <header className={classes.header}>
@@ -23,7 +25,9 @@ async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={classes.loading}>Loading...</p>}>
+          <MealsLoader />
+        </Suspense>
       </main>
     </>
   );
